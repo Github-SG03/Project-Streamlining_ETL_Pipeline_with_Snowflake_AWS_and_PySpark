a046bb3c98bed5641258518904a31443d6d9f4f2
Shivam Gupta <sgs.shivam99@outlook.com>
2025-02-17 16:54:34 +0530

    commit

--- sql_queries/snowflake_query.sql
+++ sql_queries/snowflake_query.sql
@@ -1,28 +1,27 @@
-///////////////////////////////////////////AWS Setup with Snowflake Storage Integration/////////////////////////////////////////////////////////////////////
+-----------------AWS Setup with Snowflake Storage Integration------------------------------------------------------------------/////////////////////////////////////////////////////////////////////
 --1.Create snowflake Storage Integration Object using following syntax:
 CREATE STORAGE INTEGRATION aws_s3_integration
   TYPE = EXTERNAL_STAGE
-  STORAGE_PROVIDER = S3
+  STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::767397752598:role/aws_s3_integration'
   STORAGE_ALLOWED_LOCATIONS = ('s3://sgs-s3/');
 
   
-//Run ‘DESCRIBE integration S3_INT’
+--Run ‘DESCRIBE integration S3_INT’
 DESCRIBE integration aws_s3_integration;
 
-//Now in Snowflake run following query and copy statement part of the JSON and paste it in the last to the JSON we copied from JSON preview of SNS and update SNS and save({"Sid":"1","Effect":"Allow","Principal":{"AWS":"XXXXX"},"Action":["sns:Subscribe"],"Resource":["XXXXXXX"]})
+---Now in Snowflake run following query and copy statement part of the JSON and paste it in the last to the JSON we copied from JSON preview of SNS and update SNS and save({"Sid":"1","Effect":"Allow","Principal":{"AWS":"XXXXX"},"Action":["sns:Subscribe"],"Resource":["XXXXXXX"]})
 
 
 
-///////////////////////////////////////////////////Snowflake Objects Creation Part – 1/////////////////////////////////////////////////////////////////////
+------------Snowflake Objects Creation Part – 1-----------------------------------------------------------------------------------
 --1. Create Staging Database to store schemas like table, pipes,stages....
 Create or replace database ETL_PROJECT_STAGING;
 
 --2. Create table to store covid-19 staging data
 create or replace TABLE ETL_PROJECT_STAGING.PUBLIC.COVID_19 
-(
-SNO NUMBER(38,0) autoincrement,
+( SNO NUMBER(38,0) autoincrement,
 REPORTEDDATE VARCHAR(100),
 TIME VARCHAR(100),
 STATE VARCHAR(50),
@@ -92,7 +91,7 @@ COMMENT='This is covid_19 India Data';
 
 
 
-//////////////////////////////////////////Historical Data Load Verification/////////////////////////////////////////////////////////////////////////////////
+--------------------------Historical Data Load Verification---------------------------------------------------------------------------------
 --0. Please verify the data load using below query
 SELECT * FROM TABLE(information_schema.copy_history(table_name=>'ETL_PROJECT_STAGING.PUBLIC.COVID_19',start_time=>dateadd(hours,-1,current_timestamp())));
 
@@ -107,7 +106,7 @@ Select * from ETL_PROJECT_STAGING.PUBLIC.COVID_19;
 Select * from ETL_PROJECT_FINAL.PUBLIC.COVID_19;
 
 
-////////////////////////////////////SnowFlake Objects Creation Part – 2:STAGING AND FINAL TABLE OBJECTS IN SNOWFLAKE////////////////////////////////////////
+------SnowFlake Objects Creation Part – 2:STAGING AND FINAL TABLE OBJECTS IN SNOWFLAKE--------------------------------------------------------------
 --11.0 Create Stream to capture data change when any delta data gets loaded into staging table// Note : Make append_only = true to force system to add or update new records.
 Create stream ETL_PROJECT_STAGING.PUBLIC.covid_19_stream on table ETL_PROJECT_STAGING.PUBLIC.COVID_19 append_only = true;
 
@@ -115,10 +114,10 @@ Create stream ETL_PROJECT_STAGING.PUBLIC.covid_19_stream on table ETL_PROJECT_ST
 SELECT * FROM ETL_PROJECT_STAGING.PUBLIC.covid_19_stream WHERE SNO IN (1, 2);
 
 --12. Create Task to transfer changes in staging table to final table automatically in the scheduled time i.e. (1 minute). By default, it will be in stopped status.Logic as:
-//check fields – Reported Date and State pair
-//If Reported Date and State pair is not available, then new record creation.
-//If Reported Date and State pair is available, then update data.
-//Use below query to prepare task.
+--check fields – Reported Date and State pair
+--If Reported Date and State pair is not available, then new record creation.
+--If Reported Date and State pair is available, then update data.
+--Use below query to prepare task.
 
 Create or replace task task_covid_19    
 WAREHOUSE  = compute_wh    
@@ -149,7 +148,7 @@ ALTER TASK IF EXISTS task_covid_19 RESUME;
 --14. Again Check The final table
 Select * from ETL_PROJECT_FINAL.PUBLIC.COVID_19;
 
-/////////////////////////////////Data Transformation Using PySpark on Final Table/////////////////////////////////////////////////////////////////////////
+--------------------Data Transformation Using PySpark on Final Table------------------------------------
 --Now, we will apply some transformations to the data stored in the final table and store it again in the Snowflake Analytics database. We will utilize AWS Glue to execute PySpark jobs for this purpose. The objective is to store the data in three tables: one for monthly cases, another for quarterly cases, and a third for half-yearly cases, all organized by state for each column.
 --1.Create Database 
 Create database ETL_PROJECT_Analytics;
@@ -201,13 +200,13 @@ Show Tables;
 
 
 
-////////////////////////////////////////////////////////////AWS Glue Setup for running PySpark Jobs////////////////////////////////////////////////////////
+---- -----------------------AWS Glue Setup for running PySpark Jobs--------------------------
 --To run PySpark Jobs, we need dependent Jars Spark Snowflake Connector and Snowflake Driver. Please download the dependent jars from here. We will be making use of this jar from the S3 bucket. So, please go ahead and create an S3 bucket and upload both the Jars as prerequisite.
 
-//1. In IAM > Roles > Create New Role > Choose Trusted Entity Type as AWS Service  and select Glue as Use Cases for other service in the bottom of the page
-//2. Attach policies with full access to S3 and Glue
-//3. Proceed with default actions and create role. Hence, we are done with creation of role to call other AWS Services from Glue
-//4. In AWS Glue > Jobs > Create > Job Details > Provide following details
+--1. In IAM > Roles > Create New Role > Choose Trusted Entity Type as AWS Service  and select Glue as Use Cases for other service in the bottom of the page
+--2. Attach policies with full access to S3 and Glue
+--3. Proceed with default actions and create role. Hence, we are done with creation of role to call other AWS Services from Glue
+--4. In AWS Glue > Jobs > Create > Job Details > Provide following details
   --IAM Role – Newly Created IAM Role above
   --Glue version – Glue 3.0
   --Language – Python 3
@@ -215,9 +214,9 @@ Show Tables;
   --Request number of workers – 2
   --Number of retries – 3
   --job timeout – 5 
-//5. Click on Save. We are ready to go for scripting in script tab
+--5. Click on Save. We are ready to go for scripting in script tab
 
-////////////////////////////////////////////////////////////Final output in Snowflake//////////////////////////////////////////////////////////////////////
+-------Final output in Snowflake------------------------------------------------------------------------------------------------------------------------------
 SELECT * FROM ETL_PROJECT_Analytics.PUBLIC.half_yearly_covid_report LIMIT 10;
 
 
